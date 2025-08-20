e448bc702c75cdbefa7c4eeaa8d580e7fbda2324
lakshmi bc <149359100+Lakshmibc25@users.noreply.github.com>
2025-08-20 21:20:20 +0530

    Add files via upload

---
+++ lo_practice.py
@@ -0,0 +1,23 @@
+age = int(input("enter your age:"))
+if age>18 and age<60:
+    print("True")
+else:
+    print("False")
+
+
+a = int(input("enter a value a:"))
+if a%2 ==0:
+ print("a is even")
+else:
+  print("a is odd")\
+  
+
+
+a = int(input("enter a value a:"))
+b = int(input("enter a ValueError b:"))
+
+c= a+b
+d = a-b
+e= a*b
+f=a/b
+print(c,d,e,f)
