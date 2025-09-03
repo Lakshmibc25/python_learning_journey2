d177575450a0e4c77891e4e40b6ab82cb59c4083
lakshmi bc <149359100+Lakshmibc25@users.noreply.github.com>
2025-09-03 20:09:12 +0530

    Add files via upload

---
+++ func_hw.py
@@ -0,0 +1,13 @@
+def add_numbers(a,b):
+    print(a+b)
+add_numbers(76,89)
+
+def greet():
+    print("hello goood evening bossu!")
+
+greet()
+
+def greet_user(name):
+    print(f"Hello,{name}! good evening.")
+
+greet_user("Bossu")
