ae8dbdc2114b4f49cc9d2cf41bfbc9a0237d13dd
lakshmi bc <149359100+Lakshmibc25@users.noreply.github.com>
2025-09-15 19:13:44 +0530

    Add files via upload

---
+++ practice2.dt.py
@@ -0,0 +1,15 @@
+a = int(input("enter a variable:"))
+print(type(a))
+b = float(a)
+print(b)
+print(type(b))
+
+a = float(input("enter a variable:"))
+print(a)
+b = int(a)
+print(b)
+
+a = int(input("enter integer a:"))
+b = int(input("enter integer b: "))
+add = a+b
+print(add)
