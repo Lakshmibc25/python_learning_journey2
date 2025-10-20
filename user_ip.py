2cc7eadf6d8141bcecbce7a2215da2fd0a716445
lakshmi bc <149359100+Lakshmibc25@users.noreply.github.com>
2025-10-20 18:51:48 +0530

    Add files via upload

---
+++ user_ip.py
@@ -0,0 +1,8 @@
+num1 = int(input('enter the first number: '))
+num2 = int(input('enter your second number: '))
+add = num1 + num2
+sub = num1-num2
+mul = num1 * num2
+div = num1 / num2
+print(add,sub,mul,div)
+ 
