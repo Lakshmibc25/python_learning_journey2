0fd0e08c71a7d1f4cecfdade0b751d8cea435719
lakshmi bc <149359100+Lakshmibc25@users.noreply.github.com>
2025-10-22 17:31:36 +0530

    Add files via upload

---
+++ list1.py
@@ -0,0 +1,7 @@
+fruits = ["apple","mango","banana","orange","pineapple"]
+print("second fruit is:", fruits[1])
+print("last fruit is: " ,fruits[-1])
+fruits.append("avacoda")
+print(fruits)
+fruits.remove("avacoda")
+print(fruits)
