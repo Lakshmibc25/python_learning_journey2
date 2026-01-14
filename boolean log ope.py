e35b32aeebad390fef89d2312d47dc1e4ada9a74
lakshmi bc <149359100+Lakshmibc25@users.noreply.github.com>
2026-01-14 20:35:47 +0530

    Add files via upload

---
+++ boolean log ope.py
@@ -0,0 +1,7 @@
+a = input("enter frst boolean value(True/False): ") == "True"
+b = input("enter second boolean value(True/False): ") == "True"
+
+print("a and b = ", a and b)
+print("a and b = ", a or b)
+print("a and b = ", not a)
+print("a and b = ", not b)
