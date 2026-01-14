54778b13b88e490cbcc7ad951553cc6de242c53a
lakshmi bc <149359100+Lakshmibc25@users.noreply.github.com>
2026-01-14 20:35:30 +0530

    Add files via upload

---
+++ ari_ope2.py
@@ -0,0 +1,7 @@
+num1=(int(input("enter your first number: ")))
+num2=(int(input("enter your second number:" )))
+print(num1 + num2)
+print(num1 - num2)
+print("multiplication:",num1*num2)
+print("division:",num1/num2)
+print("modulus:",num1%num2)
