d8b0e8dd373b2c7642715e67866d629e3adac162
lakshmi bc <149359100+Lakshmibc25@users.noreply.github.com>
2026-01-14 20:37:00 +0530

    Add files via upload

---
+++ comparison operators.py
@@ -0,0 +1,9 @@
+num1 = (int(input("enter you frst num: ")))
+num2 = (int(input("enter you second num: ")))
+
+if num1>num2 :
+    print("num1 is greater!")
+elif num2>num1 :
+    print("num2 is greater")
+else :
+    print("num1 and num2 are equal")
