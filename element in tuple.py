527243ea8bf260ef54e1865e9ebfa91eee3a9abd
lakshmi bc <149359100+Lakshmibc25@users.noreply.github.com>
2026-01-17 21:26:55 +0530

    Add files via upload

---
+++ element in tuple.py
@@ -0,0 +1,6 @@
+fruits = ("apple","mango","banana","orange","pineapple")
+print(fruits)
+if "apple" in fruits:
+    print("apple is present in the tuple.")
+else:
+    print("apple is not present in the tuple.")
