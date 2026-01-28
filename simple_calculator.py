dc5706eb2f9d38cf0d34fd3ed36c918c1715d2ff
lakshmi bc <149359100+Lakshmibc25@users.noreply.github.com>
2026-01-28 19:55:21 +0530

    Add files via upload

---
+++ simple_calculator.py
@@ -0,0 +1,9 @@
+num1 = (int(input("enter you frst num: ")))
+num2 = (int(input("enter you second num: ")))
+
+add = num1 + num2
+sub = num1 - num2
+mul = num1* num2
+div = num1 / num2
+
+print(add ,  sub,  mul,div)
