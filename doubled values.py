90e0a08929f8fa14da3e6367ab3e62271dce27e3
lakshmi bc <149359100+Lakshmibc25@users.noreply.github.com>
2026-02-18 19:35:13 +0530

    Add files via upload

---
+++ doubled values.py
@@ -0,0 +1,10 @@
+numbers = [1,2,3,4,5]
+doubled =[]
+for num in numbers:
+    doubled.append(num*2)
+print("doubled list:",doubled)
+
+#or
+#numbers = [1,2,3,4,5]
+#for num in numbers:
+#print(num*2)
