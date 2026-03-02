a9457aa995f364aedaa96277d977bc56b41efb34
lakshmi bc <149359100+Lakshmibc25@users.noreply.github.com>
2026-03-02 21:19:50 +0530

    Add files via upload

---
+++ lar of 3.py
@@ -0,0 +1,9 @@
+num1 = int(input("enter your 1st number: "))
+num2 = int(input("enter your 2nd number: "))
+num3 = int(input("enter your 3rd number: "))
+if num1>num2 and num>num3:
+    print("num1 is greatest.")
+elif num2>num1 and num2>num3:
+    print("num2 is greatest.")
+else:
+    print("num3 is greatest.") 
