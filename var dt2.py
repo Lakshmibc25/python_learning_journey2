74102fee1a3887978d1c6eed1c57e2b46b1ca838
lakshmi bc <149359100+Lakshmibc25@users.noreply.github.com>
2026-03-03 20:43:54 +0530

    Add files via upload

---
+++ var dt2.py
@@ -0,0 +1,7 @@
+name = input("enter your name: ")
+age = int(input("enter your age :"))
+height =float(input("enter your height:"))
+print(name,age,height)
+print(type(name))
+print(type(age))
+print(type(height))
