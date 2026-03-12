ac571d9056d6d044f0f132d7dee47673108019a3
lakshmi bc <149359100+Lakshmibc25@users.noreply.github.com>
2026-03-12 19:54:18 +0530

    Add files via upload

---
+++ equal or not.py
@@ -0,0 +1,6 @@
+num1 = int(input("enter your 1st number: "))
+num2 = int(input("enter your 2nd number: "))
+if num1 == num2:
+    print("num1 is equal to num2")
+else:
+    print("num1 and num2 are not equal")
