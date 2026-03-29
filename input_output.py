397f50f135eaa5eb427090121d99dde28275253a
lakshmi bc <149359100+Lakshmibc25@users.noreply.github.com>
2026-03-29 19:49:04 +0530

    Add files via upload

---
+++ input_output.py
@@ -0,0 +1,9 @@
+num1 = int(input("enter num1: "))
+num2 = int(input("enter num2: "))
+
+product = num1 * num2
+print(product)
+
+Name = input("enter your Name : ")
+Age = input("enter your Age : ")
+print("my name is",Name + " and my age is",Age)
