50daa9e5bcb0a289c2485fcb86eb386b7e264a1b
lakshmi bc <149359100+Lakshmibc25@users.noreply.github.com>
2026-03-30 20:12:14 +0530

    Add files via upload

---
+++ number type.py
@@ -0,0 +1,10 @@
+num = int(input("enter your number: "))
+if num%2 ==0 and num%3==0:
+    print("num is even and multiple of 3.")
+elif num % 2 == 0:
+
+    print("num is even.")
+elif num % 3 == 0:
+    print("num is multiple of 3.")
+else :
+    print("num is not even and multiple of 3.")
