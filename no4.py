d26fbd2f0de5f3a9acf171c10b2b5f8e9325891f
lakshmi bc <149359100+Lakshmibc25@users.noreply.github.com>
2026-05-08 08:55:32 +0530

    Add files via upload

---
+++ no4.py
@@ -0,0 +1,6 @@
+num1 = int(input("enter your 1st number: "))
+num2 = int(input("enter your 2nd number: "))
+sum = num1 + num2
+difference = num1-num2
+product = num1 * num2
+print(sum,difference,product)
